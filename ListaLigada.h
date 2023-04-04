87d7c2c09d1744cd9caf6bdfc4d4d49909a6a632
j-falcao2002 <joaorpalharesfalcao@gmail.com>
2023-04-04 18:04:38 +0100

    Melhoria de funcoes para remover e adicionar ao fim/inicio

--- ListaLigada.h
+++ ListaLigada.h
@@ -15,7 +15,9 @@ typedef struct{
 LG *criarLG();
 NOG *criarNOG(void *elemento);
 void AddNOG_fim(LG *lista, NOG *elemento);
-NOG *RemNOG_inicio(LG *lista);                      // Devolve o NOG removido para o podermos detruir e adicionar info ao historico
+NOG *RemNOG_inicio(LG *lista);
+NOG *RemNOG_ultimo(LG *lista);
+NOG *RemNOG_index(LG *lista, int index);              // Devolve o NOG removido para o podermos detruir e adicionar info ao historico
 void MostrarLG(LG *lista, void (MostrarInfo)(void *));
 void DestruirLG(LG *lista, void (DestruirInfo)(void *));
 
--- ListaLigadaFuncoes.c
+++ ListaLigadaFuncoes.c
@@ -1,10 +1,10 @@
 #include "ListaLigada.h"
 #include "TipoDados.h"
 
-
 LG *criarLG(){
     LG *NovaLG = (LG*) malloc(sizeof(LG));
     NovaLG->head = NULL;
+    NovaLG->tail = NULL;
     NovaLG->n_el = 0;
     return NovaLG;
 }
@@ -18,20 +18,79 @@ NOG *criarNOG(void *elemento){
     return NovoNOG;
 }
 
+void AddNOG_inicio(LG *lista, NOG *elemento){
+    if(!lista || !elemento) return;
+
+    if(!lista->head)
+        lista->tail = elemento;
+
+    elemento->next = lista->head;
+    lista->head = elemento;
+    lista->n_el++;
+}
+
+
 void AddNOG_fim(LG *lista, NOG *elemento){
     if(!lista || !elemento) return;
+
+    if(!lista->head)
+        AddNOG_inicio(lista, elemento);
 
     lista->tail->next = elemento;
     lista->tail = elemento;
     lista->n_el++;
 }
 
-NOG *RemNOG_inicio(LG *lista){                  // Devolve o NOG removido para o podermos detruir e adicionar info ao historico
-    if(!lista) return NULL;                     // Fazer mais generico
+NOG *RemNOG_ultimo(LG *lista){
+    if(!lista) return NULL;
 
-    NOG *NOG_Removido = lista->head;
+    NOG *Aux = lista->head, *removido = lista->tail;
+    while(Aux->next->next)
+        Aux = Aux->next;
+
+    lista->tail = Aux;
+    Aux->next = NULL;
+    lista->n_el--;
+    return removido;
+}
+
+NOG *RemNOG_inicio(LG *lista){
+    if(!lista) return NULL;
+
+    NOG *removido = lista->head;
     lista->head = lista->head->next;
-    return NOG_Removido;
+    lista->n_el;
+    return removido;
+}
+
+NOG *RemNOG_index(LG *lista, int index){          // Devolve o NOG removido para o podermos detruir e adicionar info ao historico
+    if(!lista || index >= lista->n_el) return NULL;    
+
+    NOG *removido;
+    if(lista->n_el < 3){
+        if(index)
+            removido = RemNOG_ultimo(lista);
+        else
+            removido = RemNOG_inicio(lista);
+    }
+
+    NOG *prev = lista->head, *curr = lista->head->next->next;
+    int index_c = 2;
+    while(index_c <= index && curr){
+        prev = prev->next;
+        curr = curr->next;
+        index_c++;
+    }
+
+    if(!curr)
+        removido = RemNOG_ultimo(lista);
+    else if(lista->head == prev)
+        removido = RemNOG_inicio(lista);
+    else{
+        removido = prev->next;
+        prev->next = prev->next->next;
+        lista->n_el--;
+    }
 }
 
 void MostrarLG(LG *lista, void (MostrarInfo)(void *)){
