4f3322539f699cea152eee84cfabf388c1940e32
Benno Tadeu <bennovasconcellos@gmail.com>
2023-04-20 22:28:43 +0100

    blab

--- src/relogio.c
+++ src/relogio.c
@@ -16,7 +16,7 @@
 void *ThreadTempo(){
     while(1){
         time_t tempoTot;
-        //struct tm * timeinfo;
+        struct tm * timeinfo;
         time (&tempoTot);
         timeinfo = localtime (&tempoTot);
         //printf ( "Current local time and date: %s", asctime (timeinfo) );
