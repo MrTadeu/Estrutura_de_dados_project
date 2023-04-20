df1cbff2961dc2a1378d02aaf2d68740c76be269
GamerPedro4K <pedro.t.carvalho12345@gmail.com>
2023-04-20 22:47:31 +0100

    A

--- includes/TipoDados.h
+++ includes/TipoDados.h
@@ -1,127 +1,110 @@
 #ifndef TIPODADOS_H_INCLUDED
-#define TIPODADOS_H_INCLUDED
+    #define TIPODADOS_H_INCLUDED
 
-#include "../includes/ListaLigada.h"
-#include <stdio.h>
-#include <stdlib.h> 
-#include <string.h>
-#include <math.h>
-#include <time.h>     // Para contar o tempo
-#ifndef PTHREAD_H
-#include <pthread.h> 
-#endif // Para criar as threads
-//#include <conio.h>
- 
-#ifdef _WIN32
-#include <windows.h>
-#endif
+    #include "../includes/ListaLigada.h"
+    #include <stdio.h>
+    #include <stdlib.h> 
+    #include <string.h>
+    #include <math.h>
+    #include <time.h>
 
-#ifdef __linux__
-#include <linux.h>
-#endif
-
-
-typedef struct{
-    char *nome;
-    int id, experiencia, ativo;                // A experiencia é a quantidade de vendas realizadas e influencia o salário.
-    float atrasoMedio, bonus, salario;  // O atraso medio pode ser negativo ou positivo e influencia o bonus.
-}FuncionarioStruct;
-
-typedef struct{
-    int id, tempoTotalEspera, aberta;
-    FuncionarioStruct *funcionario;
-    Lista *listaPessoas;
-}CaixaStruct;
-
-typedef struct{
-    int codigo;
-    char *nome;
-    float preco, tempoCompra, tempoCaixa;
-}ProdutoStruct;
-
-typedef struct{
-    int dia, mes, ano;
-}DataStruct;
-
-typedef struct{
-    int id, ativo;                      // guests: -1
-    char *nome;                         // guests: "none"
-    DataStruct dataNascimento;          // guests: -1/-1/-1
-    float saldoCartaoCliente;           // guests: -1 | clientes: angariado a cada compra e pode ser usado em qualquer uma das compras
-
-    int tempoEstimadoCompra;        // soma do tempo de compra dos proprios produtos
-    int tempoEstimadoFila;          // soma do tempo dos produtos das pessoas a sua frente no momento em que entrou na fila
-    int tempoEstimadoCaixa;         // soma do tempo de caixa dos proprios produtos
-
-    int tempoAtraso;                // soma do atraso aleatorio de cada pessoa a sua frente
-
-    Lista *listaProdutos;           // lista dos produtos
-}ClienteStruct;
+    //Criar as threads
+    #ifndef PTHREAD_H
+        #include <pthread.h> 
+    #endif 
+    
+    //TIME (SLEEP)
+    #ifdef _WIN32
+        #include <windows.h>
+    #endif
+    #ifdef __linux__
+        #include <linux.h>
+    #endif
+    
+    //Printcolor
+    #ifndef CJAG_COLORPRINT
+        #define CJAG_COLORPRINT
+        #define ERROR_TAG "[r][ERROR][/r] "
+        void printc(const char *fmt, ...);
+        void spinner_start(unsigned int type, const char* fmt, ...);
+        void spinner_update(const char* fmt, ...);
+        void spinner_done(const char* fmt, ...);
+        void progress_start(int max, char* fmt);
+        void progress_update();
+    #endif
 
 
-//GLOBAL VARIABLES
-extern ClienteStruct *Clientes;
-extern FuncionarioStruct *Funcionarios;
-extern ProdutoStruct *Produtos;
-extern int n_clientes, n_clientesAtivos, n_funcionarios, n_funcionariosAtivos, n_produtos;
+    typedef struct{
+        char *nome;
+        int id, experiencia, ativo;                // A experiencia é a quantidade de vendas realizadas e influencia o salário.
+        float atrasoMedio, bonus, salario;  // O atraso medio pode ser negativo ou positivo e influencia o bonus.
+    }FuncionarioStruct;
 
-typedef enum {
-    CLIENTES,
-    FUNCIONARIOS
-} TipoDados;
+    typedef struct{
+        int id, tempoTotalEspera, aberta;
+        FuncionarioStruct *funcionario;
+        Lista *listaPessoas;
+    }CaixaStruct;
 
-void importarClientesx(char **linhaString, int n_linha);
-void importarFuncionariosx(char **linhaString, int n_linha);
-void importarDados(char *filename, int colunas, void (guardarDados)(char **, int), TipoDados tipo);
+    typedef struct{
+        int codigo;
+        char *nome;
+        float preco, tempoCompra, tempoCaixa;
+    }ProdutoStruct;
 
+    typedef struct{
+        int dia, mes, ano;
+    }DataStruct;
 
+    typedef struct{
+        int id, ativo;                      // guests: -1
+        char *nome;                         // guests: "none"
+        DataStruct dataNascimento;          // guests: -1/-1/-1
+        float saldoCartaoCliente;           // guests: -1 | clientes: angariado a cada compra e pode ser usado em qualquer uma das compras
 
-int escolherAleatorioVetor(void *vetor, int batente, int tamanhoVetor, size_t tamanhoElemento, void *ptrElemento); // Funcional
-void associarProdutosCliente(ClienteStruct *cliente, Lista *produtos);
-ClienteStruct *criarGuest();
-void batenteChange(void* ptr1, void* ptr2, size_t size, int *batente, char sinal);
-void *escolherFuncionarios();
-void *escolherProduto();
-void *escolherCliente();
-void criarAddLista(Lista *lista, int Qt, void*(*escolherElemento)());
+        int tempoEstimadoCompra;        // soma do tempo de compra dos proprios produtos
+        int tempoEstimadoFila;          // soma do tempo dos produtos das pessoas a sua frente no momento em que entrou na fila
+        int tempoEstimadoCaixa;         // soma do tempo de caixa dos proprios produtos
 
-void mostrarFuncionario(void *funcionarioArg, int indentLevel);
-void mostrarCaixa(void *caixaArg, int indentLevel);
-void mostrarProduto(void *produtoArg, int indentLevel);
-void mostrarCliente(void *clienteArg, int indentLevel);
+        int tempoAtraso;                // soma do atraso aleatorio de cada pessoa a sua frente
 
-DataStruct gerarDataValidaAleatoria(int anoMin, int anoMax); // Funcional
-int DataAntes_Depois(DataStruct d1, DataStruct d2); // Funcional
+        Lista *listaProdutos;           // lista dos produtos
+    }ClienteStruct;
 
+     typedef enum {
+        CLIENTES,
+        FUNCIONARIOS
+    } TipoDados;
 
-//Funções importar_exportar
-ClienteStruct* importarClientes(int* totalClientes, char *nomeficheiro); // Funcional
-FuncionarioStruct* importarFuncionarios(int* totalClientes, char *nomeficheiro); // Funcional
-ProdutoStruct* importarProdutos(int* totalClientes, char *nomeficheiro); // Funcional
+    //GLOBAL VARIABLES
+    extern ClienteStruct *Clientes;
+    extern FuncionarioStruct *Funcionarios;
+    extern ProdutoStruct *Produtos;
+    extern int n_clientes, n_clientesAtivos, n_funcionarios, n_funcionariosAtivos, n_produtos;
 
-//Utils.c
-int Aleatorio(int min, int max);
+    //ImportExport.c
+    void importarClientesx(char **linhaString, int n_linha);
+    void importarFuncionariosx(char **linhaString, int n_linha);
+    void importarDados(char *filename, int colunas, void (guardarDados)(char **, int), TipoDados tipo);
 
+    //TipoDadosFuncoes.c
+    int escolherAleatorioVetor(void *vetor, int batente, int tamanhoVetor, size_t tamanhoElemento, void *ptrElemento); // Funcional
+    void associarProdutosCliente(ClienteStruct *cliente, Lista *produtos);
+    ClienteStruct *criarGuest();
+    void *escolherFuncionarios();
+    void *escolherProduto();
+    void *escolherCliente();
+    void batenteChange(void* ptr1, void* ptr2, size_t size, int *batente, char sinal);
+    void mostrarFuncionario(void *funcionarioArg, int indentLevel);
+    void mostrarCaixa(void *caixaArg, int indentLevel);
+    void mostrarProduto(void *produtoArg, int indentLevel);
+    void mostrarCliente(void *clienteArg, int indentLevel);
 
+    //Utils.c
+    int Aleatorio(int min, int max);
+    DataStruct gerarData(int anoMin, int anoMax);
+    int DataAntesDepois(DataStruct d1, DataStruct d2);
 
-/* <-------------------------------- Printcolor --------------------------------> */
-#ifndef CJAG_COLORPRINT
-#define CJAG_COLORPRINT
-
-#define ERROR_TAG "[r][ERROR][/r] "
-
-void printc(const char *fmt, ...);
-
-void spinner_start(unsigned int type, const char* fmt, ...);
-void spinner_update(const char* fmt, ...);
-void spinner_done(const char* fmt, ...);
-
-void progress_start(int max, char* fmt);
-void progress_update();
-
-#endif
-
-/* <-------------------------------- /Printcolor --------------------------------> */
-
-
+    //JOAOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO????????????????
+    //void criarAddLista(Lista *lista, int Qt, void*(*escolherElemento)());
 #endif
--- src/Utils.c
+++ src/Utils.c
@@ -4,3 +4,56 @@ int Aleatorio(int min, int max){
     int resultado = min + rand() % (max-min + 1);
     return resultado;
 }
+
+DataStruct gerarData(int anoMin, int anoMax){
+    DataStruct data;
+    data.ano = Aleatorio(anoMin, anoMax);
+    data.mes = Aleatorio(1, 12);
+
+    if((data.mes % 2 != 0 && data.mes < 8) || (data.mes >= 8 && data.mes % 2 == 0))
+        data.dia = Aleatorio(1, 31);
+    else if((data.mes % 2 == 0 && data.mes <= 8 && data.mes != 2) || (data.mes > 8 && data.mes % 2 != 0))
+        data.dia = Aleatorio(1, 30);
+    else if((data.mes == 2 && data.ano % 4 == 0))
+        data.dia = Aleatorio(1, 29);
+    else if((data.mes == 2 && data.ano % 4 != 0))
+        data.dia = Aleatorio(1, 28);
+    
+    return data;
+}
+
+int DataAntesDepois(DataStruct d1, DataStruct d2){ // Se d1 for antes de d2 return 1
+    if(d2.ano > d1.ano || 
+        (d2.ano == d1.ano && 
+        d2.mes > d1.mes) || 
+        (d2.ano == d1.ano && 
+        d2.mes == d1.mes && 
+        d2.dia >=  d1.dia))
+        return 1;
+    return 0;
+}
+
+
+
+/* int fileExists(const char* path) {
+  struct stat st;
+  if (stat(path, &st) == 0) {
+    return 1;
+  }
+  return 0;
+}
+
+int fileExistAll(){
+  if(fileExists("data/bin/alunos.bin") && fileExists("data/bin/cursosdisciplina.bin") && fileExists("data/bin/disciplinas.bin") && fileExists("data/bin/permission.bin") && fileExists("data/bin/professores.bin") && fileExists("data/bin/regimes.bin") && fileExists("data/bin/salas.bin")){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+} */
+
+
+/* if (fileExistAll() == 0){
+        printc("[red]Error[/red] Ficheiros não existem.\n\n");
+        MenuFilesNovos();
+    } */
