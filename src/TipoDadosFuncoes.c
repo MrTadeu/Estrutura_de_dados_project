e429a1f371a795497da35ea02e16804c3da78f3f
GamerPedro4K <pedro.t.carvalho12345@gmail.com>
2023-04-20 22:48:40 +0100

    A

--- src/TipoDadosFuncoes.c
+++ src/TipoDadosFuncoes.c
@@ -119,33 +119,3 @@ void mostrarCliente(void *clienteArg, int indentLevel){
         indentLevel*4, " ", cliente->tempoEstimadoFila, indentLevel*4, " ", cliente->tempoEstimadoCaixa, indentLevel*4, " ", cliente->tempoAtraso);
     mostrarLista(cliente->listaProdutos, mostrarProduto, indentLevel + 1);
 }
-
-DataStruct gerarDataValidaAleatoria(int anoMin, int anoMax){
-    DataStruct data;
-    data.ano = Aleatorio(anoMin, anoMax);
-    data.mes = Aleatorio(1, 12);
-
-    if((data.mes % 2 != 0 && data.mes < 8) || (data.mes >= 8 && data.mes % 2 == 0))
-        data.dia = Aleatorio(1, 31);
-    else if((data.mes % 2 == 0 && data.mes <= 8 && data.mes != 2) || (data.mes > 8 && data.mes % 2 != 0))
-        data.dia = Aleatorio(1, 30);
-    else if((data.mes == 2 && data.ano % 4 == 0))
-        data.dia = Aleatorio(1, 29);
-    else if((data.mes == 2 && data.ano % 4 != 0))
-        data.dia = Aleatorio(1, 28);
-    
-    return data;
-}
-
-int DataAntes_Depois(DataStruct d1, DataStruct d2){ // Se d1 for antes de d2 return 1
-    if(d2.ano > d1.ano || 
-        (d2.ano == d1.ano && 
-        d2.mes > d1.mes) || 
-        (d2.ano == d1.ano && 
-        d2.mes == d1.mes && 
-        d2.dia >=  d1.dia))
-        return 1;
-    return 0;
-}
-
-
