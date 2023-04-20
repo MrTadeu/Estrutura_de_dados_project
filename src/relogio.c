6347c1e3adccbc320ab29ef2a1549bac9709a538
Benno Tadeu <bennovasconcellos@gmail.com>
2023-04-20 21:58:28 +0100

    relogio update

--- src/relogio.c
+++ src/relogio.c
@@ -1,5 +1,5 @@
+#include "TipoDados.h"
 #ifdef _WIN32
-#include <windows.h>
     void dormir(){
         Sleep(1000);
     }   
@@ -7,12 +7,21 @@
 
 
 #ifdef __linux__
-#include <linux.h>
     void dormir(){
-        usleep(1000);
+        usleep(1000000);
     }
 #endif
 
 
+void *ThreadTempo(){
+    while(1){
+        time_t tempoTot;
+        struct tm * timeinfo;
+        time (&tempoTot);
+        timeinfo = localtime (&tempoTot);
+        //printf ( "Current local time and date: %s", asctime (timeinfo) );
+        dormir();
+    }
+}
 
   
