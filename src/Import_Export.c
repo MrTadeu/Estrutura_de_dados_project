704a51ef45645c507b1581374be38152edc86b87
GamerPedro4K <pedro.t.carvalho12345@gmail.com>
2023-04-20 22:27:45 +0100

    a

--- src/Import_Export.c
+++ src/Import_Export.c
@@ -1,12 +1,5 @@
 #include "../includes/TipoDados.h"
 
-//GLOBAL VARIABLES
-/* extern ClienteStruct *Clientes;
-extern FuncionarioStruct *Funcionarios;
-extern ProdutoStruct *Produtos;
-extern int n_clientes, n_funcionarios, n_produtos; */
-
-
 /* 
 int main(){
     importarDados("../Data/clientes.txt", 5, importarClientesx, CLIENTES);
@@ -55,7 +48,6 @@ int importarCount(char *filename){
 
         n_linhas++;
     }
-
     fclose(file);
     return n_linhas;
 }
@@ -113,113 +105,3 @@ void importarDados(char *filename, int colunas, void (guardarDados)(char **, int
 
     fclose(file);
 }
-
-
-
-
-
-/* 
-
-
-
-ClienteStruct* importarClientes(int* totalClientes, char* nomeficheiro){
-    char linha[1000], *buffer;
-    int i = 0;
-    *totalClientes = 0;
-    
-    FILE* f = fopen(nomeficheiro, "r");
-    if(!f){
-        printf("\tError: cant open file\n");
-        return NULL;
-    }
-    fgets(linha, sizeof(linha), f); //Para limpar lixo da primeira linha
-    
-    fgets(linha, sizeof(linha), f);
-    sscanf(linha, "%d", totalClientes);
-    ClienteStruct* dadosImportados = (ClienteStruct*) malloc(*totalClientes * sizeof(ClienteStruct));
-
-    while(fgets(linha, sizeof(linha), f)){
-        dadosImportados[i].id = atoi(strtok(linha, ","));
-
-        buffer = strtok(NULL, "\n");
-        strncpy(dadosImportados[i].nome, buffer, sizeof(dadosImportados[i].nome)-1); // A guardar o nome do cliente
-        dadosImportados[i].nome[sizeof(dadosImportados[i].nome)-1] = '\0';
-
-        i++;
-    }
-    return dadosImportados;
-}
-
-FuncionarioStruct* importarFuncionarios(int* totalFuncionarios, char *nomeficheiro){
-    char linha[1000], *buffer;
-    int i = 0;
-    *totalFuncionarios = 0;
-    
-    FILE* f = fopen(nomeficheiro, "r");
-    if(!f){
-        printf("\tError: cant open file\n");
-        return NULL;
-    }
-    fgets(linha, sizeof(linha), f); //Para limpar lixo da primeira linha
-    
-    fgets(linha, sizeof(linha), f);
-    sscanf(linha, "%d", totalFuncionarios);
-    FuncionarioStruct* dadosImportados = (FuncionarioStruct*) malloc(*totalFuncionarios * sizeof(FuncionarioStruct));
-
-    while(fgets(linha, sizeof(linha), f)){
-        dadosImportados[i].id = atoi(strtok(linha, ","));
-
-        buffer = strtok(NULL, "\n");
-        strncpy(dadosImportados[i].nome, buffer, sizeof(dadosImportados[i].nome)-1); // A guardar o nome do cliente
-        dadosImportados[i].nome[sizeof(dadosImportados[i].nome)-1] = '\0';
-
-        i++;
-    }
-    return dadosImportados;
-}
-
-ProdutoStruct* importarProdutos(int* totalProdutos, char *nomeficheiro){
-    char linha[1000], *buffer;
-    int i = 0;
-    *totalProdutos = 0;
-    
-    FILE* f = fopen(nomeficheiro, "r");
-    if(!f){
-        printf("\tError: cant open file\n");
-        return NULL;
-    }
-    fgets(linha, sizeof(linha), f); //Para limpar lixo da primeira linha
-    
-    fgets(linha, sizeof(linha), f);
-    sscanf(linha, "%d", totalProdutos);
-    ProdutoStruct* dadosImportados = (ProdutoStruct*) malloc(*totalProdutos * sizeof(ProdutoStruct));
-
-    while(fgets(linha, sizeof(linha), f)){
-        dadosImportados[i].codigo = atoi(strtok(linha, ","));
-
-        buffer = strtok(NULL, ",");
-        strncpy(dadosImportados[i].nome, buffer, sizeof(dadosImportados[i].nome)-1); // A guardar o nome do cliente
-        dadosImportados[i].nome[sizeof(dadosImportados[i].nome)-1] = '\0';
-
-        buffer = strtoDataStruct random_date(int start_year, int end_year)k(NULL, ",");
-        sscanf(buffer, "%f", &dadosImportados[i].preco);
-
-        buffer = strtok(NULL, ",");
-        sscanf(buffer, "%f", &dadosImportados[i].tempoCompra);
-
-        buffer = strtok(NULL, "\n");
-        sscanf(buffer, "%f", &dadosImportados[i].tempoCaixa);
-
-        i++;
-    }
-    return dadosImportados;
-} */
-
-
-
-
-
-
-
-
-
