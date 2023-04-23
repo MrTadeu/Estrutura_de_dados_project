ec81003846abca5acc502d932ba9c4bed48f31f6
Benno Tadeu <bennovasconcellos@gmail.com>
2023-04-23 17:41:19 +0100

    kjyu

---
+++ src/menu.c
@@ -0,0 +1,28 @@
+#include "../includes/TipoDados.h"
+
+void menu(){
+    int opcao = -1;
+    do{
+        int opcao;
+        printf("**************************************************\n");
+        printf("***************    Menu Principal    *************\n");
+        printf("**************************************************\n");
+        printf("**        0 -> Sair                             **\n");
+        printf("**        1 -> OOOOOOOOOOO                      **\n");
+        printf("**        2 -> OOOOOOOOOOOO                     **\n");
+        printf("**        3 -> OOOOOOOO                         **\n");
+        printf("**************************************************\n");
+        printf("Qual a opção que pretende? ");
+        scanf("%d", &opcao);
+    } while (opcao != 0);
+    
+}
+[/blue]
+[/blue]
+[/blue]
+[/blue]
+
+[blue]
+[blue]
+[blue]
+[blue]
