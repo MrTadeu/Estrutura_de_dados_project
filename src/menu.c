1f0dda39485f088b0f1dcd6257110f448ab67243
Benno Tadeu <bennovasconcellos@gmail.com>
2023-04-23 19:02:50 +0100

    asd

--- src/menu.c
+++ src/menu.c
@@ -1,28 +1,45 @@
 #include "../includes/TipoDados.h"
 
 void menu(){
-    int opcao = -1;
     do{
-        int opcao;
+        int opcao, j = 0;
         printf("**************************************************\n");
         printf("***************    Menu Principal    *************\n");
         printf("**************************************************\n");
-        printf("**        0 -> Sair                             **\n");
-        printf("**        1 -> OOOOOOOOOOO                      **\n");
-        printf("**        2 -> OOOOOOOOOOOO                     **\n");
-        printf("**        3 -> OOOOOOOO                         **\n");
+        printf("**        [blue]%d [/blue]-> Sair                             **\n", j++);
+        printf("**        [blue]%d [/blue]-> Configurações                    **\n", j++);
+        printf("**        [blue]%d [/blue]-> Menu Funcionarios                **\n", j++);
+        printf("**        [blue]%d [/blue]-> Ver Historico                    **\n", j++);
         printf("**************************************************\n");
         printf("Qual a opção que pretende? ");
         scanf("%d", &opcao);
+
+        int j = 0;
+        switch (opcao){
+            case j++:
+                printf("A sair...\n");
+                break;
+            case j++:
+                /* menuConfiguracoes(); */
+                break;
+            case j++:
+                /* menuFuncionarios(); */
+                break;
+            case j++:
+                /* menuHistorico(); */
+                break;
+            default:
+                printf("Opção inválida!\n");
+                break;
+        }
     } while (opcao != 0);
     
 }
-/* [/blue]
-[/blue]
-[/blue]
-[/blue]
 
-[blue]
-[blue]
-[blue]
-[blue] */
+
+
+
+
+
+
+
