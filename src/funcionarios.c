c9fa2576cf020d6c5d4f4883a691e9c2a576f5dc
Benno Tadeu <bennovasconcellos@gmail.com>
2023-04-25 14:32:05 +0100

    asd

--- src/funcionarios.c
+++ src/funcionarios.c
@@ -23,12 +23,26 @@ void verFuncionariosCaixa(){
 void verFuncionariosInativos(){
     fputs("\x1b[H\x1b[2J\x1b[3J", stdout);
     for (int i = 0; i < n_funcionarios; i++){
-        printf("\nID: %d Nome: %s Salario: %.2f€  Nivel de Experiencia: %d\n", Funcionarios[i].id, Funcionarios[i].nome, Funcionarios[i].salario, Funcionarios[i].experiencia);
+        printf("\nID: %d Nome: %s Salario: %.2f€\n", Funcionarios[i].id, Funcionarios[i].nome, Funcionarios[i].salario);
     }
     getchar();
     getchar();
 }
 
 void pesquisarFuncionarios(){
-
+    int id;
+    fputs("\x1b[H\x1b[2J\x1b[3J", stdout);
+    printf("Insira o ID do funcionario que pretende pesquisar: ");
+    scanf("%d", &id);
+    int pos = encontrarIdFuncionario(id);
+    if (pos == -1){
+        printf("Funcionario não encontrado!");
+        getchar();
+        getchar();
+    }
+    else{
+        printf("\nID: %d Nome: %s Salario: %.2f€\n", Funcionarios[pos].id, Funcionarios[pos].nome, Funcionarios[pos].salario);
+        getchar();
+        getchar();
+    }
 }
