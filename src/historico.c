24ccb404bca706b8ebe1d73db0d6647024740c1f
j-falcao2002 <joaorpalharesfalcao@gmail.com>
2023-05-25 17:17:32 +0100

    d

--- src/historico.c
+++ src/historico.c
@@ -34,8 +34,8 @@ int hashFunction(char *nome){
     int sum = 0;
     int p = 31;  // Um número primo para gerar o hash
 
-    for (int i = 0; s[i] != '\0'; i++) {
-        sum = (sum * p) + s[i];
+    for (int i = 0; nome[i] != '\0'; i++) {
+        sum = (sum * p) + nome[i];
     }
 
     // Ajusta o tamanho do hash para o tamanho do vetor
@@ -65,7 +65,7 @@ void *criarInfoHistorico(CaixaStruct *caixa, float movimentoSaldoCartao, float v
     infoHistorico->movimentoCartaoCliente = movimentoSaldoCartao;
     infoHistorico->precoTotal = pessoa->precoTotalProdutos;
     infoHistorico->valorProdutoOferecido = valorProdutoOferecido;
-    infoHistorico->dataTransacao = convertTimeDate(getDataMilliseconds()); 
+    infoHistorico->dataTransacao = formatTimeStruct(getDataMilliseconds()); 
     // infoHistorico.hora
     return infoHistorico;
 }
@@ -125,9 +125,29 @@ void mostrarHistorico(){
         Elemento* clientesHistorico = HistoricoDados.HistoricoTransacoes[i]->head;
         while(clientesHistorico){
             HistoricoSubStructCliente* clientesHistoricoInfo = (HistoricoSubStructCliente*) clientesHistorico->Info;
-            printc("[green]Nome cliente:[/green] %s\n[green]ID cliente:[/green] %d", clientesHistorico->nome, clientesHistorico->id);
-            for (int i = 0; i < Opcoes.numCaixasTotal; i++){
-                
+            for (int j = 0; j < Opcoes.numCaixasTotal; j++){
+                Elemento* caixasHistorico = clientesHistoricoInfo->caixas[j]->head;
+                while(caixasHistorico){
+                    HistoricoSubStructInfo* caixasHistoricoInfo = (HistoricoSubStructInfo*) caixasHistorico->Info;
+                    printc("\n[blue]Nome cliente:[/blue] %s\t[blue]ID cliente:[/blue] %d\n[blue]ID caixa:[/blue] %d\t[blue]Nome funcionário:[/blue] %d\t[blue]ID funcionário:[/blue] %d\n[blue]Data de transação:[/blue] %d/%d/%d\t[blue]Hora: %d:%d:%d[/blue]\n[blue]Tempo de espera na fila:[/blue] %s\t", clientesHistoricoInfo->nome, clientesHistoricoInfo->id, j, caixasHistoricoInfo->funcionario->nome, caixasHistoricoInfo->funcionario->id, caixasHistoricoInfo->dataTransacao.dia, caixasHistoricoInfo->dataTransacao.mes, caixasHistoricoInfo->dataTransacao.ano, caixasHistoricoInfo->dataTransacao.hora, caixasHistoricoInfo->dataTransacao.minuto, caixasHistoricoInfo->dataTransacao.segundo, formatTime(caixasHistoricoInfo->tempoEstimadoCaixa));
+
+                    printc("[blue]Tempo de atraso:[/blue] ");
+                    if(caixasHistoricoInfo->tempoAtraso < 0)
+                        printc("[green]Adiantou-se:[/green] %.2f segundos\n", (float)caixasHistoricoInfo->tempoAtraso/1000.0);
+                    else if(caixasHistoricoInfo->tempoAtraso > 0)
+                        printc("[red]Atrasou-se:[/red] %.2f segundos\n", (float)caixasHistoricoInfo->tempoAtraso/1000.0);
+
+                    Elemento* produtos = caixasHistoricoInfo->listaProdutos->head;
+                    while(produtos){
+                        ProdutoStruct* produtoInfo = (ProdutoStruct*) produtos->Info;
+                        printc("\n\t[blue]ID produto:[/blue] %d [blue]Nome produto:[/blue] %s, [blue]QT:[/blue] %dX, [blue]Preco:[/blue] %.2f [blue]TCompra:[/blue] %d [blue]TCaixa:[/blue] %d",produtoInfo->id, produtoInfo->nome, produtoInfo->quantidadeProdutosRepetidos, produtoInfo->preco, produtoInfo->tempoCompra, produtoInfo->tempoCaixa );
+                        produtos = produtos->next;
+                    }
+
+                    
+
+                    caixasHistorico = caixasHistorico->next;
+                }
             }
             
             clientesHistorico = clientesHistorico->next;
